830f68f9362599eff99a055571922ed646d27ac0
Andreas Harmuth <andreas@harmuth.io>
2018-05-07 20:54:06 +0200

    how to die of cancer

--- src/SignAnalysis.java
+++ src/SignAnalysis.java
@@ -1,48 +1,100 @@
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
-public class Interpreter extends GCLBaseVisitor<Object> {
+public class SignAnalysis extends GCLBaseVisitor<Object> {
     Node lastNode;
     private boolean debug = false;
     private boolean running = true;
     private ArrayList<Edge> edgeList;
 
-    private Map<String, Integer> mem = new HashMap<String, Integer>();
 
-    public Interpreter(ArrayList<Edge> edgeList){
+    private Map<String, String> currentAbstractMem = new HashMap<>();
+
+    private String pElements[] = {"+"};
+    Set<String> setP = new HashSet<>(Arrays.asList(pElements));
+    private String pmElements[] = {"+","-"};
+    Set<String> setPM = new HashSet<>(Arrays.asList(pmElements));
+    private String pmzElements[] = {"+","-","+"};
+    Set<String> setPMZ = new HashSet<>(Arrays.asList(pmzElements));
+    
+
+    private final String[][][] assignPower;
+
+
+
+    private Map<Node,Set<Map<String, String>>> mem = new HashMap<>();
+    
+    private SignAnalysis(ArrayList<Edge> edgeList){
         this.edgeList = edgeList;
+        /*
+
+          + 0 - x1
+        -
+        0
+        +
+        x2
+        */
+        this.assignPower = new String[][][]{{new String[] {"+"}, new String[] {"0"},new String[] {"u"}},
+                                            {new String[] {"1"}, new String[] {"1"},new String[] {"1"}},
+                                            {new String[] {"+"}, new String[] {"0"},new String[] {"+","-"}}};
+
+
     }
 
     public void setDebug(boolean debug){
         this.debug = debug;
     }
 
+    // Chaotic
+    private void start(){
+        Set<Map<String, String>> EmptySet;
+
+        // for all q -> A(q):={}
+        for(Edge e : edgeList){
+            EmptySet = new HashSet<>();
+            this.mem.putIfAbsent(e.fromNode,EmptySet);
+        }
+
+
+        Edge e = edgeList.get(0);
+
+        // Set starting memory
+        Set<Map<String, String>> StartMemSet = new HashSet<>();
+        StartMemSet.add(currentAbstractMem);
+        this.mem.put(e.fromNode,StartMemSet);
+        boolean chaoticRun = true;
+        while(chaoticRun){
+            for(Edge edge : edgeList){
+                for(Map<String, String> abstractMem : this.mem.get(edge.fromNode)){
+                    this.currentAbstractMem = abstractMem;
+                    visit(edge.edge);
+                }
+
+            }
+        }
+        this.lastNode = visitEdge(e,0);
+
+
+    }
+
 
     public String toString(){
         System.out.print(lastNode.getPosition() == -1?"Terminated ":"Stuck ");
         String s = "";
-        for (Map.Entry<String, Integer> entry : mem.entrySet())
-        {
-            System.out.print(entry.getKey() + " " + entry.getValue() + " ");
+        for(Map.Entry<Node,Set<Map<String, String>>> allAbstractMem : this.mem.entrySet()){
+            System.out.println(allAbstractMem.getKey());
+            for(Map<String,String> abstractMem : allAbstractMem.getValue()){
+                for (Map.Entry<String, String> entry : abstractMem.entrySet())
+                {
+                    System.out.println(entry.getKey() + " " + entry.getValue() + " ");
+                }
+            }
+
         }
 
         return "";
     }
-
-
-    private Map<String, Integer> getMeThatSweetMemory(){
-
-        // O(n)
-        Edge e = edgeList.get(0);
-        this.lastNode = visitEdge(e,0);
-
-        return this.mem;
-    }
-
 
     private boolean runDone(ParseTree ctx){
 
@@ -58,59 +110,82 @@ public class Interpreter extends GCLBaseVisitor<Object> {
 
     private Node visitEdge(Edge e,int i){
         Node currentNode = e.fromNode;
-        if (debug) System.out.println(e);
 
-        Edge eNext = null;
-        int nextI = i;
+            if (debug) System.out.println(e);
 
-        boolean b;
+            Edge eNext = null;
+            int nextI = i;
 
-        if (e.runDone) {
-            b = runDone(e.edge);
-        } else {
-            b = (boolean) visit(e.edge);
-        }
+            boolean b;
+
+            if (e.runDone) {
+                b = runDone(e.edge);
+            } else {
+                b = (boolean) visit(e.edge);
+            }
 
 
-        if(this.running) {
-            if (!b) {
+            if(this.running) {
 
-                for (int j = i + 1; j < edgeList.size(); j++) {
-                    if (edgeList.get(j).fromNode == currentNode) {
-                        eNext = edgeList.get(j);
-                        nextI = j;
-                        break;
+                // TODO Remove b?
+                if (!b) {
+                    // TODO: mem2-> mem1, mem2
+                    for (int j = i + 1; j < edgeList.size(); j++) {
+                        if (edgeList.get(j).fromNode == currentNode) {
+                            eNext = edgeList.get(j);
+                            nextI = j;
+                            break;
+                        }
+
+                    }
+                } else {
+                    for (int j = 0; j < edgeList.size(); j++) {
+                        if (edgeList.get(j).fromNode == e.toNode) {
+                            eNext = edgeList.get(j);
+                            nextI = j;
+                            break;
+                        }
+
                     }
 
                 }
-            } else {
-                for (int j = 0; j < edgeList.size(); j++) {
-                    if (edgeList.get(j).fromNode == e.toNode) {
-                        eNext = edgeList.get(j);
-                        nextI = j;
-                        break;
-                    }
 
+                if (eNext != null) {
+                    currentNode = visitEdge(eNext, nextI);
+                } else {
+                    currentNode = e.toNode;
                 }
-
+                return currentNode;
             }
-
-            if (eNext != null) {
-                currentNode = visitEdge(eNext, nextI);
-            } else {
-                currentNode = e.toNode;
-            }
+        return currentNode;
         }
-            return currentNode;
+
+
+    private String getSign(int x){
+        return x>0?"+":x<0?"-":"0";
     }
 
+
+    private Set<String> getSignsAsSet(String x1,String x2, String[][][] table){
+        Set<String> rSet = new HashSet<>();
+        
+        if(x1.equals("u") || x2.equals("u")){
+            rSet.add("u");
+        }
+        else {
+            int x1i = x1.equals("-")?0:x1.equals("0")?1:2;
+            int x2i = x2.equals("-")?0:x2.equals("0")?1:2;
+            rSet.addAll(Arrays.asList(table[x1i][x2i]));
+        }
+        return rSet;
+    }
 
     @Override
     public Object visitStart(GCLParser.StartContext ctx) {
 
         return null;
     }
-
+    /*
     @Override public Object visitBoolDouble(GCLParser.BoolDoubleContext ctx) {
         switch(ctx.getChild(1).getText()){
             case "&":
@@ -142,7 +217,6 @@ public class Interpreter extends GCLBaseVisitor<Object> {
                 return ((int)visit(ctx.getChild(0)))<((int)visit(ctx.getChild(2)));
             case "<=":
                 return ((int)visit(ctx.getChild(0)))<=((int)visit(ctx.getChild(2)));
-
         }
         return null;
     }
@@ -170,13 +244,13 @@ public class Interpreter extends GCLBaseVisitor<Object> {
 
         return visit(ctx.getChild(1));
     }
-
+    */
 
     @Override public Object visitAritVar(GCLParser.AritVarContext ctx) {
 
-        mem.putIfAbsent(ctx.getText(),0); // Init with 0
+        // TODO: what if abscent? mem.putIfAbsent(ctx.getText(),)
 
-        return mem.get(ctx.getText());
+        return currentAbstractMem.get(ctx.getText());
     }
 
 
@@ -203,6 +277,7 @@ public class Interpreter extends GCLBaseVisitor<Object> {
 
     @Override public Object visitAritPower(GCLParser.AritPowerContext ctx) {
         int a = (int) visit(ctx.getChild(0));
+
         int b = (int) visit(ctx.getChild(2));
         return a^b;
     }
@@ -226,7 +301,7 @@ public class Interpreter extends GCLBaseVisitor<Object> {
         // TODO: The x+1 paradox?
 
         if(visit(ctx.getChild(2))!=null&&(int)visit(ctx.getChild(2))!=0){
-            System.out.println("Stop"+(int)visit(ctx.getChild(2)));
+
             return (int)visit(ctx.getChild(2))/(int)visit(ctx.getChild(2));
         }
 
@@ -238,9 +313,8 @@ public class Interpreter extends GCLBaseVisitor<Object> {
     @Override
     public Object visitCAssign(GCLParser.CAssignContext ctx) {
 
-        // return null on 1/0?
 
-        if (visit(ctx.getChild(2))!=null)this.mem.put(ctx.getChild(0).getText(),(int) visit(ctx.getChild(2)));
+        if (visit(ctx.getChild(2))!=null)this.currentAbstractMem.put(ctx.getChild(0).getText(),getSign((int) visit(ctx.getChild(2))));
 
         return true;
     }
